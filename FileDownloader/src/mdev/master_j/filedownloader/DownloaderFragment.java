cd59fb186411249f87f2a2815a95a77b15b6fb25
Noname Sorry <noemail@sor.ry>
2014-03-27 19:04:11 +0400

    +empty actionButtonClickListener

--- FileDownloader/src/mdev/master_j/filedownloader/DownloaderFragment.java
+++ FileDownloader/src/mdev/master_j/filedownloader/DownloaderFragment.java
@@ -4,11 +4,19 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 public class DownloaderFragment extends Fragment {
 	private Button actionButton;
+
+	private OnClickListener actionButtonClickListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			actionButton.setEnabled(false);
+		}
+	};
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -21,6 +29,7 @@ public class DownloaderFragment extends Fragment {
 		super.onActivityCreated(savedInstanceState);
 
 		actionButton = (Button) getActivity().findViewById(R.id.button_action);
-
+		actionButton.setOnClickListener(actionButtonClickListener);
+		actionButton.setText(R.string.button_action_download);
 	}
 }
