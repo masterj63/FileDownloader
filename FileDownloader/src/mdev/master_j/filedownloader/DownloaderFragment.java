8d34353472fe11525f0ac4c32e0cef3450776a82
Noname Sorry <noemail@sor.ry>
2014-03-27 15:00:03 +0400

    draft commit

--- FileDownloader/src/mdev/master_j/filedownloader/DownloaderFragment.java
+++ FileDownloader/src/mdev/master_j/filedownloader/DownloaderFragment.java
@@ -1,10 +1,18 @@
 package mdev.master_j.filedownloader;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class DownloaderFragment extends Fragment {
 	@Override
@@ -12,4 +20,42 @@ public class DownloaderFragment extends Fragment {
 		View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 		return rootView;
 	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		Button b = (Button) getActivity().findViewById(R.id.button_action);
+		b.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				onActionButtonClick();
+			}
+		});
+	}
+
+	private void onActionButtonClick() {
+		String url = "http://developer.android.com/design/media/new_widgets.png";
+		ConnectivityManager connMgr = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo netInfo = connMgr.getActiveNetworkInfo();
+		if (netInfo != null && netInfo.isConnected()) {
+			new Downloader().execute(url);
+		} else {
+			Toast.makeText(getActivity(), "no connection", Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	private class Downloader extends AsyncTask<String, Void, Bitmap> {
+		@Override
+		protected Bitmap doInBackground(String... urls) {
+			return downloadBitmap(urls[0]);
+		}
+
+		@Override
+		protected void onPostExecute(Bitmap result) {
+			super.onPostExecute(result);
+			ImageView img = (ImageView) getActivity().findViewById(R.id.imageView1);
+
+		}
+	}
+
 }
