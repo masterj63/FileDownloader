d0d62873dde7e5beaa0fb6653dac844b4bf2d12a
Noname Sorry <noemail@sor.ry>
2014-03-27 18:20:45 +0400

    works betta

--- FileDownloader/src/mdev/master_j/filedownloader/DownloaderFragment.java
+++ FileDownloader/src/mdev/master_j/filedownloader/DownloaderFragment.java
@@ -1,28 +1,34 @@
 package mdev.master_j.filedownloader;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 public class DownloaderFragment extends Fragment {
+	private static final String PICTURE_NAME = "pic.jpg";
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_main, container, false);
@@ -42,7 +48,10 @@ public class DownloaderFragment extends Fragment {
 	}
 
 	private void onActionButtonClick() {
-		String url = "http://developer.android.com/design/media/new_widgets.png";
+		Log.d("mj>>>", "clicked");
+		// String url =
+		// "http://www.freedomwallpaper.com//nature-wallpaper-hd/6.jpg";
+		String url = "http://developer.android.com/images/home/kk-hero.jpg";
 		ConnectivityManager connMgr = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
 		NetworkInfo netInfo = connMgr.getActiveNetworkInfo();
 		if (netInfo != null && netInfo.isConnected()) {
@@ -52,20 +61,43 @@ public class DownloaderFragment extends Fragment {
 		}
 	}
 
-	private class Downloader extends AsyncTask<String, Void, Bitmap> {
+	private class Downloader extends AsyncTask<String, Void, Void> {
 		@Override
-		protected Bitmap doInBackground(String... urls) {
-			return downloadBitmap(urls[0]);
+		protected Void doInBackground(String... urls) {
+			downloadBitmap(urls[0]);
+			return null;
 		}
 
 		@Override
-		protected void onPostExecute(Bitmap result) {
-			super.onPostExecute(result);
-			ImageView img = (ImageView) getActivity().findViewById(R.id.imageView1);
-			if (result == null)
-				Toast.makeText(getActivity(), "null bitmap", Toast.LENGTH_SHORT).show();
-			else
-				img.setImageBitmap(result);
+		protected void onPostExecute(Void v) {
+			super.onPostExecute(v);
+
+			File file_t = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "mdev");
+			if (!file_t.mkdirs()) {
+				Log.d("mj>>>", "Directory not created");
+			}
+
+			File galDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "mdev");
+			File picFile = new File(galDir.getAbsoluteFile() + "/" + PICTURE_NAME);
+
+			Log.d("mj>>>", picFile.getAbsolutePath());
+			Log.d("mj>>>", "pic exists : " + picFile.exists());
+
+			if (!picFile.exists()) {
+				Toast.makeText(getActivity(), "no pic found", Toast.LENGTH_SHORT).show();
+				return;
+			}
+
+			Uri uri = Uri.fromFile(picFile);
+			Log.i("mj>>>", picFile.getAbsolutePath());
+			Log.i("mj>>>", uri.getPath());
+
+			Intent intent = new Intent();
+			intent.setAction(Intent.ACTION_VIEW);
+			intent.setDataAndType(uri, "image/jpg");
+			startActivity(intent);
+
+			Log.d("mj>>>", "opened");
 		}
 	}
 
@@ -83,21 +115,30 @@ public class DownloaderFragment extends Fragment {
 			// start
 			conn.connect();
 			int response = conn.getResponseCode();
-			Log.d("DEBUG TAG>>", "The response is: " + response);
+			Log.d("mj>>>", "The response is: " + response);
 			ins = conn.getInputStream();
+			// save
 
-			Bitmap res = readIt(ins);
-			return res;
+			File galDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "mdev");
+			File picFile = new File(galDir.getAbsolutePath() + "/" + PICTURE_NAME);
+			Log.d("mj>>>", "streaming file to : " + picFile.getAbsolutePath() + " ;  exists : " + picFile.exists());
+			OutputStream ous = new FileOutputStream(picFile);
+			int t;
+			while ((t = ins.read()) != -1)
+				ous.write(t);
+			ous.flush();
+			ous.close();
+			Log.d("mj>>>", "saved; ous closed.");
+			Log.d("mj>>>", "streamed file to : " + picFile.getAbsolutePath() + " ;  exists : " + picFile.exists());
 		} catch (MalformedURLException e) {
+			Log.d("mj>>>", "MalformedURLException");
 			e.printStackTrace();
 		} catch (IOException e) {
+			Log.d("mj>>>", "IOException");
+			for (StackTraceElement s : e.getStackTrace())
+				Log.d("mj>>>", ">" + s.toString());
 			e.printStackTrace();
 		}
 		return null;
-	}
-
-	private Bitmap readIt(InputStream ins) {
-		Bitmap res = BitmapFactory.decodeStream(ins);
-		return res;
 	}
 }
---
+++ FileDownloader/src/mdev/master_j/filedownloader/MyProvider.java
@@ -0,0 +1,50 @@
+package mdev.master_j.filedownloader;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
+
+public class MyProvider extends ContentProvider {
+
+	@Override
+	public ParcelFileDescriptor openFile(Uri uri, String mode) throws FileNotFoundException {
+		File privateFile = new File(getContext().getFilesDir(), uri.getPath());
+		return ParcelFileDescriptor.open(privateFile, ParcelFileDescriptor.MODE_READ_ONLY);
+	}
+
+	@Override
+	public boolean onCreate() {
+		return false;
+	}
+
+	@Override
+	public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+		return null;
+	}
+
+	@Override
+	public String getType(Uri uri) {
+		return null;
+	}
+
+	@Override
+	public Uri insert(Uri uri, ContentValues values) {
+		return null;
+	}
+
+	@Override
+	public int delete(Uri uri, String selection, String[] selectionArgs) {
+		return 0;
+	}
+
+	@Override
+	public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+		return 0;
+	}
+
+}
