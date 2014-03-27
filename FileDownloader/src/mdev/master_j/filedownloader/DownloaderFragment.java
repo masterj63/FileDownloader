49ac9e9d2c1e85b179935a6d3f7e6fc63d2db494
Noname Sorry <noemail@sor.ry>
2014-03-27 15:41:42 +0400

    +actionButton labels

--- FileDownloader/src/mdev/master_j/filedownloader/DownloaderFragment.java
+++ FileDownloader/src/mdev/master_j/filedownloader/DownloaderFragment.java
@@ -5,11 +5,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class DownloaderFragment extends Fragment {
+	private Button actionButton;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 		return rootView;
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+
+		actionButton = (Button) getActivity().findViewById(R.id.button_action);
+
 	}
 }
