9b4111e9ca21a911d2d0910c0bc3be92b3ca34b4
Noname Sorry <noemail@sor.ry>
2014-03-27 19:32:14 +0400

    +basic UI updating; +empy PictureDownloaderAsyncTask

--- FileDownloader/src/mdev/master_j/filedownloader/DownloaderFragment.java
+++ FileDownloader/src/mdev/master_j/filedownloader/DownloaderFragment.java
@@ -1,20 +1,31 @@
 package mdev.master_j.filedownloader;
 
 import android.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class DownloaderFragment extends Fragment {
 	private Button actionButton;
+	private TextView statusTextView;
+	private boolean downloading;
+	private boolean downloaded;
 
 	private OnClickListener actionButtonClickListener = new OnClickListener() {
 		@Override
 		public void onClick(View v) {
-			actionButton.setEnabled(false);
+			if (!downloaded) {
+				// open
+			} else {
+				new PictureDownloaderAsyncTask().execute();
+			}
+			updUI();
 		}
 	};
 
@@ -28,8 +39,58 @@ public class DownloaderFragment extends Fragment {
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 
+		downloading = false;
+		downloaded = false;
+
 		actionButton = (Button) getActivity().findViewById(R.id.button_action);
 		actionButton.setOnClickListener(actionButtonClickListener);
 		actionButton.setText(R.string.button_action_download);
+
+		statusTextView = (TextView) getActivity().findViewById(R.id.status_textView);
+	}
+
+	private void updUI() {
+		if (!downloading && !downloaded) {
+			statusTextView.setText(R.string.status_textview_idle);
+			actionButton.setText(R.string.button_action_download);
+			actionButton.setEnabled(true);
+			return;
+		}
+		if (downloading && !downloaded) {
+			statusTextView.setText(R.string.status_textview_loading);
+			actionButton.setText(R.string.button_action_download);
+			actionButton.setEnabled(false);
+			return;
+		}
+		if (!downloading && downloaded) {
+			statusTextView.setText(R.string.status_textview_loaded);
+			actionButton.setText(R.string.button_action_open);
+			actionButton.setEnabled(true);
+			return;
+		}
+		if (downloading && downloaded) {
+			String text = "both downloading and downloaded are true";
+			IllegalStateException exception = new IllegalStateException(text);
+			Log.d("mj_tag", text, exception);
+			throw exception;
+		}
+	}
+
+	private class PictureDownloaderAsyncTask extends AsyncTask<Void, Void, Void> {
+		@Override
+		protected Void doInBackground(Void... params) {
+			downloading = true;
+			downloaded = false;
+			updUI();
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			downloading = false;
+			downloaded = true;
+			updUI();
+			super.onPostExecute(result);
+		}
 	}
 }
