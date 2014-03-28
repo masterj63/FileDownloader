fbe554bcd589b383ae38acd20c5b7397a70b737c
Noname Sorry <noemail@sor.ry>
2014-03-28 12:31:07 +0400

    DownloaderFragment is retained now

--- FileDownloader/src/mdev/master_j/filedownloader/DowloaderActivity.java
+++ FileDownloader/src/mdev/master_j/filedownloader/DowloaderActivity.java
@@ -1,21 +1,26 @@
 package mdev.master_j.filedownloader;
 
+import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
 public class DowloaderActivity extends ActionBarActivity {
+	private static final String TAG_DOWNLOADER_FRAGMENT = "mdev.master_j.filedownloader.DownloaderFragment";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		if (savedInstanceState == null) {
-			FragmentManager fragmentManager = getFragmentManager();
+		FragmentManager fragmentManager = getFragmentManager();
+		Fragment fragment = fragmentManager.findFragmentByTag(TAG_DOWNLOADER_FRAGMENT);
+		if (fragment == null) {
+			fragment = new DownloaderFragment();
+			fragment.setRetainInstance(true);
 			FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-			fragmentTransaction.add(R.id.layout_activity_main, new DownloaderFragment());
+			fragmentTransaction.add(R.id.layout_activity_main, fragment, TAG_DOWNLOADER_FRAGMENT);
 			fragmentTransaction.commit();
 		}
 	}
